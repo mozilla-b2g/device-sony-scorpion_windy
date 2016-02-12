a331bb6c3462acb41b1e263bab807642e9222430
Adam Farden <adam@farden.cz>
2016-02-12 18:14:58 +0100

    We welcome Scorpion WiFi to the B2G community!
    Signed-off-by: Adam Farden <adam@farden.cz>

--- extract-files.sh
+++ extract-files.sh
@@ -1,5 +1,6 @@
-#
-# Copyright 2014 The Android Open Source Project
+#!/bin/bash
+
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo scorpion_windy-userdebug
+DEVICE=scorpion
+MANUFACTURER=sony
+STOCK=23.4.A.1.264
+
+. ../common/extract-files.sh
